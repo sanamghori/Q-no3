c10b51e02c25822b03c98d9ca58c166b6dfaeee5
sanamghori <161497237+sanamghori@users.noreply.github.com>
2024-06-04 22:26:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.//
+var firstName = "wamiq khan";
+console.log(firstName.toLocaleLowerCase());
+console.log(firstName.toLocaleUpperCase());
+console.log(firstName.charAt(0).toLocaleUpperCase() + firstName.slice(1));
